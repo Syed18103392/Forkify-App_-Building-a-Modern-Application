8c4cd2c2f214df912a0cf223b9ab00cdf3bf8703
SyedSajib <56638411+Syed18103392@users.noreply.github.com>
2022-06-12 00:10:10 +0600

    set timeout for fetch data

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,30 +6,21 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function(){ 
   try{
     const id = window.location.hash.slice(1);
-    if(!id)return;
+    if(!id) return;
 
   recipeView.renderSpinner();
   
   // 1) rendeding pizza info 
     await model.loadRecipe(id);
+
     const {recipe}  = model.state; 
+    console.log(recipe);
     // 2) Rendering recipe 
-   recipeView.render(model.state.recipe);
+    recipeView.render(recipe);
     
         }catch(e){
           alert(e);
--- src/js/model.js
+++ src/js/model.js
@@ -1,17 +1,14 @@
 import {async} from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
 export const state = {
     recipe  :   {}
 }
 
 export const loadRecipe = async function(id){
     try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-        const data = await res.json();
-        // console.log(res,data);
 
-        if (!res.ok) {
-            throw new Error(`${data.message} status code ${res.status}`);
-        }
+        const data = await getJSON(`${API_URL}/${id}`);
 
         const { recipe } = data.data;
         state.recipe = {
@@ -25,7 +22,7 @@ export const loadRecipe = async function(id){
             ingredients: recipe.ingredients
         }
     }
-    catch(e){
-        alert(e);
+    catch(err){
+        console.error(`${err} 💥💥💥`);
     }
 }
